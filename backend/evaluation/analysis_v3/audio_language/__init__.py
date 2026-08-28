9c6b1c2def6fe4ff4e0d57809bfe3655ffe2aafb
gr-rr <giancarloricci319@gmail.com>
2026-08-28 11:27:12 -0700

    eval(audio-language): add evaluation package

---
+++ backend/evaluation/analysis_v3/audio_language/__init__.py
@@ -0,0 +1,5 @@
+"""Analysis V3 audio-language evaluation utilities."""
+
+from .metrics import grounded_value_gate, score_assessments, score_by_condition
+
+__all__ = ["grounded_value_gate", "score_assessments", "score_by_condition"]
