8df37df8f9593202ccdf92c4cdd6d2264830d0a7
gr-rr <giancarloricci319@gmail.com>
2026-08-27 23:26:43 -0700

    design: load V6 visual interaction layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./workspace-v3.css";
 import "./product-polish-v4.css";
 import "./workspace-interactions.css";
 import "./visual-language-v5.css";
+import "./visual-language-v6.css";
 import { Geist } from "next/font/google";
 import MSWInit from "@/components/MSWInit";
 import AuthProvider from "@/components/AuthProvider";
