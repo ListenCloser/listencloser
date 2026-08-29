4ea060ee3c812f776cce51da7b6a7d671b75f21c
gr-rr <giancarloricci319@gmail.com>
2026-08-29 14:54:24 -0700

    eval(structure): register verified SongFormer adapter

--- backend/evaluation/analysis_v3/structure/adapters/__init__.py
+++ backend/evaluation/analysis_v3/structure/adapters/__init__.py
@@ -1,9 +1,11 @@
 from .allin1 import AllInOneStructureAdapter
 from .base import StructureAdapter, StructureMetadata, StructureResult
 from .external_json import ExternalJsonStructureAdapter
+from .songformer import SongFormerStructureAdapter
 
 ADAPTERS: dict[str, type[StructureAdapter]] = {
     "allin1": AllInOneStructureAdapter,
+    "songformer": SongFormerStructureAdapter,
     "external_json": ExternalJsonStructureAdapter,
 }
 
@@ -11,6 +13,7 @@ __all__ = [
     "ADAPTERS",
     "AllInOneStructureAdapter",
     "ExternalJsonStructureAdapter",
+    "SongFormerStructureAdapter",
     "StructureAdapter",
     "StructureMetadata",
     "StructureResult",
