bbed5467e786f5c960c1af9950d5e84a442da049
gr-rr <giancarloricci319@gmail.com>
2026-08-29 19:21:12 -0700

    fix: register corrected MIDI entity sync

--- backend/worker.py
+++ backend/worker.py
@@ -5,6 +5,7 @@ import os
 import signal
 
 import domain.capabilities as capability_module
+from domain.correction_entity_sync import register_corrected_midi_entity_sync
 from domain.job_worker import JobWorker
 from domain.perceptual_capability import register_perceptual_capability
 from domain.performance_instrumentation import install_understand_instrumentation
@@ -40,6 +41,7 @@ def main() -> None:
     worker = JobWorker(max_workers=int(os.environ.get("WORKER_CONCURRENCY", "1")))
     install_understand_instrumentation(capability_module)
     capability_module.register_all_capabilities(worker)
+    register_corrected_midi_entity_sync(worker)
     register_perceptual_capability(worker)
 
     def stop(_signum, _frame) -> None:
